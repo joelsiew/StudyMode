475002a774e772348607ff634a48eeedddff993c
Musab Abdullah <musababdullah6231@gmail.com>
2019-11-02 15:44:28 -0500

    added blank login and registration templates and loginform

--- studymode/routes.py
+++ studymode/routes.py
@@ -1,6 +1,19 @@
 from studymode import app
-from flask import url_for, render_template
+from flask import render_template
+from studymode.forms import LoginForm
+
 
 @app.route('/')
 def hello_world():
     return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
