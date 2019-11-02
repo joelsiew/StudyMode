5f330d54350f82016fc81dbbb4756318927a48bf
Adam Bouker <abogo00@gmail.com>
2019-11-02 16:49:56 -0500

    initial commit from adam

--- studymode/__init__.py
+++ studymode/__init__.py
@@ -1,5 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'ur mom',
+        'title': "how u doing",
+        'content': 'good shit'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 
 from studymode import routes
