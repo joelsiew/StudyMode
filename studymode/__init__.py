792457396dc70c16497e8f3dc8ebb330c92c802e
Musab Abdullah <musababdullah6231@gmail.com>
2019-11-02 16:48:36 -0500

    added map to map page

--- studymode/__init__.py
+++ studymode/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask.ext.googlemaps import GoogleMaps
-
+from flask_googlemaps import GoogleMaps
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyDXtCfWA1EuXPc4geNQTm2NsIb8xllcCac"
 GoogleMaps(app)
 
 from studymode import routes
--- studymode/routes.py
+++ studymode/routes.py
@@ -1,9 +1,12 @@
 from studymode import app
 from flask import url_for, render_template
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
 @app.route('/map')
 def draw_map():
     return render_template('map.html')
