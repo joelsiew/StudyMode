d72ac12078512485cda02eb1c616fa6ab9d5c3cd
joelsiew <siew.joel@utexas.edu>
2019-11-02 17:36:01 -0500

    Adds EventForm

--- studymode/forms.py
+++ studymode/forms.py
@@ -1,10 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length
-
+from wtforms import StringField, SubmitField, PasswordField, IntField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=10)])
     submit = SubmitField('Log In')
 
+class EventForm(FlaskForm):
+    course = StringField('Course',
+                         validators = [DataRequired(), Length(min = 1)])
+    address = StringField('Address',)
+
+    locked = BooleanField('Locked', )
+
+    password = PasswordField('Password', validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators = [DataRequired(), EqualTo('password')])
+    submit = SubmitField('Submit')
