9ec54632844dfe837cc4ff3e533cdee2e594e32d
RickyTiet <rpt446@utexas.edu>
2019-11-02 19:20:26 -0500

    Made login/ registration form

--- studymode/forms.py
+++ studymode/forms.py
@@ -1,20 +1,42 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, IntField, BooleanField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username",
+                           validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField("Email",
+                        validators=[DataRequired(), Length(min=2, max=20)])
+
+    password = PasswordField("Password",
+                             validators=[DataRequired()])
+
+    confirm_password = PasswordField("Confirm Password",
+                                     validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Register')
+
+
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=10)])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=2, max=20)])
+
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
 
 class EventForm(FlaskForm):
     course = StringField('Course',
-                         validators = [DataRequired(), Length(min = 1)])
-    address = StringField('Address',)
+                         validators=[DataRequired(), Length(min=1)])
+    address = StringField('Address', )
 
     locked = BooleanField('Locked', )
 
-    password = PasswordField('Password', validators = [DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators = [DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Submit')
