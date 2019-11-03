ba668efe3346b3e5e83d81acca154dbcdb483ce8
Dristi Patel <dristipatel@gmail.com>
2019-11-02 19:29:33 -0500

    map.py

--- studymode/routes.py
+++ studymode/routes.py
@@ -1,8 +1,7 @@
 from studymode import app
 from flask import url_for, render_template
-from flask_googlemaps import Map, icons
-from .forms import RegistrationForm, LoginForm, EventForm
 import geocoder
+from studymode.map import draw_map
 
 
 @app.route('/')
@@ -11,28 +10,16 @@ def home():
 
 
 @app.route('/map')
-def draw_map():
-    g = geocoder.ip('me')
-    latitude, longitude = g.latlng[0], g.latlng[1]
-    studymap = Map(
-        identifier="study",
-        varname="studymap",
-        style="height:720px;width:1100px;margin:0;",  # hardcoded!
-        lat=latitude,
-        lng=longitude,
-        zoom=15,
-        markers=[]
-    )
+def map():
+    studymap = draw_map()
     return render_template('map.html', studymap=studymap)
 
 
 @app.route('/register')
 def register():
-    form = RegistrationForm()
-    return render_template('register.html', title="Registration", form=form)
+    return render_template('register.html')
+
 
 @app.route('/login')
 def login():
-    form = LoginForm()
-    return render_template('login.html', title="Login", form=form)
-
+    return render_template('login.html')
