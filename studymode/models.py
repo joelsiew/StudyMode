b9e374b5e1a6cbe78a7e3bc65aa8f359fbded91d
joelsiew <siew.joel@utexas.edu>
2019-11-02 19:13:24 -0500

    Adds Location Class

--- studymode/models.py
+++ studymode/models.py
@@ -9,3 +9,9 @@ class Event(db.Model):
     class_name = db.Column(db.String, nullable = False)
     private_event = db.Column(db.Boolean, nullable = False, default = False)
     private_password = db.Column()
+
+class Location(db.Model):
+    longitude = db.Column(db.Float(min=-180, max=180, nullable=False))
+    latitude = db.Column(db.Float(min=-85.05112878, max=85.05112878), nullable=False)
+    address = db.Column(db.String, nullable=False)
+    zipcode = db.Column(db.Integer(min=00501, max=99950), nullable=False)
