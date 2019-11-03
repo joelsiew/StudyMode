201e177f51326ab82a1f1ba0a5a38bad32979787
Musab Abdullah <musababdullah6231@gmail.com>
2019-11-02 19:22:41 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/study_mode.iml
    #	studymode/models.py

--- studymode/models.py
+++ studymode/models.py
@@ -1,6 +1,19 @@
 from studymode import db
 from datetime import datetime
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    events = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     start_time = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
