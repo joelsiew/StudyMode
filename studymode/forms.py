abae50e12b9921adeb49a48808830e4e06d38144
vijayvuyyuru <vijaykvuyyuru@gmail.com>
2019-11-03 04:44:13 -0600

    added time functionality and adjusted events listing

--- studymode/forms.py
+++ studymode/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
-from wtforms_components import TimeField
+from wtforms.fields.html5 import DateTimeLocalField
 
 
 class RegistrationForm(FlaskForm):
@@ -32,4 +32,6 @@ class LoginForm(FlaskForm):
 
 class EventForm(FlaskForm):
     course = StringField('Course', validators=[DataRequired(), Length(min=1)])
+    start_time_input = DateTimeLocalField('Enter Start Time', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
+    end_time_input = DateTimeLocalField('Enter End Time', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
     submit = SubmitField('Submit')
