058cb988f1b9a1a2627448e53c7bcbd2649d172c
Musab Abdullah <musababdullah6231@gmail.com>
2019-11-04 16:36:19 -0600

    hid api key and added config.py to gitignore

---
+++ config.py
@@ -0,0 +1,2 @@
+api_key = 'AIzaSyDXtCfWA1EuXPc4geNQTm2NsIb8xllcCac'
+secret = '5791628bb0b13ce0c676dfde280ba245'
--- studymode/__init__.py
+++ studymode/__init__.py
@@ -3,11 +3,11 @@ from flask_googlemaps import GoogleMaps, Map
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-
+import config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['GOOGLEMAPS_KEY'] = "AIzaSyDXtCfWA1EuXPc4geNQTm2NsIb8xllcCac"
+app.config['SECRET_KEY'] = config.secret
+app.config['GOOGLEMAPS_KEY'] = config.api_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 GoogleMaps(app)
 db = SQLAlchemy(app)
